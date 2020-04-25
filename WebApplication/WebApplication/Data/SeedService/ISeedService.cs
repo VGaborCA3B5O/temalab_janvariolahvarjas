702a04e991d4cdd8c7c070bd27f2a0828b0caf8b
VGaborCA3B5O <gvarjas00@gmail.com>
2020-04-25 18:41:01 +0200

    entities and seedservice

---
+++ WebApplication/WebApplication/Data/SeedService/ISeedService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Data.Entities;
+
+namespace WebApplication.Data.SeedService
+{
+    public interface ISeedService
+    {
+        IDictionary<string, User> Users { get; }
+        IList<Post> Posts { get; set; }
+    }
+}
