874984320f1fd98a54461c2997f883a5b16416a7
Zsolt Olah <agaxy0000@gmail.com>
2020-04-28 14:34:13 +0200

    Migrations

---
+++ WebApplication/WebApplication/Migrations/20200428122947_Seed.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication.Migrations
+{
+    public partial class Seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "a6bd9fdd-23df-4590-8a9b-15ecf7cc8ddb");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "b1687477-f4e8-4024-8e16-160ad297a583");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "e11d006e-323f-43b6-b114-9d69401a5e03");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "a9868b2f-fe6d-4f27-b06f-890495c363db");
+        }
+    }
+}
