1e1f161030f9188a84315ec1e016f4dff1f3cc3c
Zsófia Jánvári <janvari.zsofi@gmail.com>
2020-05-17 14:17:21 +0200

    Update Create page to be able to save new post

--- WebApplication/WebApplication/Pages/Post/Create.cshtml.cs
+++ WebApplication/WebApplication/Pages/Post/Create.cshtml.cs
@@ -1,10 +1,13 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using WebApplication.Data.Entities;
 
 namespace WebApplication.Pages.Post
 {
@@ -12,11 +15,13 @@ namespace WebApplication.Pages.Post
     {
         private readonly Data.ApplicationDbContext _context;
         private readonly IWebHostEnvironment hostingEnvironment;
+        private readonly UserManager<User> _userManager;
 
-        public CreateModel(Data.ApplicationDbContext context, IWebHostEnvironment environment)
+        public CreateModel(Data.ApplicationDbContext context, IWebHostEnvironment environment, UserManager<User> userManager)
         {
             _context = context;
             this.hostingEnvironment = environment;
+            _userManager = userManager;
         }
 
         public IActionResult OnGet()
@@ -35,21 +40,18 @@ namespace WebApplication.Pages.Post
         // more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
+            if (!ModelState.IsValid || this.Image == null)
             {
                 return Page();
             }
 
-            if (this.Image != null)
-            {
-                string fileName = "7" + Path.GetExtension(Image.FileName); // next id ?
-                string filePath = Path.Combine(hostingEnvironment.WebRootPath, "Images", fileName);
-                this.Image.CopyTo(new FileStream(filePath, FileMode.Create));
-                this.Post.ImageName = fileName;
-            }
-
-            _context.Posts.Add(Post);
-            await _context.SaveChangesAsync(); // itt a baj
+            string fileName = Guid.NewGuid().ToString() + Path.GetExtension(Image.FileName);
+            string filePath = Path.Combine(hostingEnvironment.WebRootPath, "Images", fileName);
+            this.Image.CopyTo(new FileStream(filePath, FileMode.Create));
+            this.Post.ImageName = fileName;
+            this.Post.User = await _userManager.GetUserAsync(HttpContext.User);
+            _context.Posts.Add(Post);       
+            await _context.SaveChangesAsync();
             return RedirectToPage("./Index");
         }
     }
