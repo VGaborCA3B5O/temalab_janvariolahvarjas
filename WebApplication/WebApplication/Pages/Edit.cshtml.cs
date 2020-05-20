6f610671795b37f1a1a4e69602f8bfc435fda248
Zsófia Jánvári <janvari.zsofi@gmail.com>
2020-05-20 21:13:23 +0200

    Now admin can edit any post

--- WebApplication/WebApplication/Pages/Edit.cshtml.cs
+++ WebApplication/WebApplication/Pages/Edit.cshtml.cs
@@ -47,7 +47,7 @@ namespace WebApplication.Pages.Post
             {
                 return NotFound();
             }
-            else if (Post.UserId.ToString() != _userManager.GetUserId(User))
+            else if (Post.UserId.ToString() != _userManager.GetUserId(User) && !User.IsInRole("Administrators"))
             {
                 return Redirect("./Identity/Account/AccessDenied");
             }
