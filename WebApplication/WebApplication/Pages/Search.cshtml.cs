e45279238bade20c81588ddcc47da1670bd21ffd
Zsófia Jánvári <janvari.zsofi@gmail.com>
2020-05-20 18:44:33 +0200

    Update search page

--- WebApplication/WebApplication/Pages/Search.cshtml.cs
+++ WebApplication/WebApplication/Pages/Search.cshtml.cs
@@ -11,31 +11,30 @@ namespace WebApplication
     public class SearchModel : PageModel
     {
 
-        public IEnumerable<WebApplication.Data.Entities.Post> Post { get; private set; }
+        public IEnumerable<Data.Entities.Post> Posts { get; private set; }
+        public List<Data.Entities.Post> FoundPosts { get; private set; } = new List<Data.Entities.Post>();
         public Data.Entities.Post p { get; private set; }
         [BindProperty(SupportsGet = true)]
         public string SearchString { get; set; }
         public string b;
 
         public SearchModel([FromServices]PostService postService)
-        {           
-            Post = postService.GetPosts();
+        {
+            Posts = postService.GetPosts();
         }
 
         public async Task OnGetAsync()
         {
             if (SearchString != null)
             {
-                foreach (var post in Post)
+                foreach (var post in Posts)
                 {
-                    if (post.Title == SearchString)
+                    if (post.Title.IndexOf(SearchString, StringComparison.OrdinalIgnoreCase) >= 0)
                     {
-                        p = post;
-                        break;
+                        FoundPosts.Add(post);
                     }
                 }
             }
         }
-
-        }
+    }
 }
